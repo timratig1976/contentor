db37288be97d2cc7c1b9ebf8a71127390b776901
Tim Ratig <timratig@gmail.com>
2026-09-07 23:06:09 +0200

    Implement Content Transfer Functionality with Data Export and Import Commands

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -10,6 +10,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             StrategySeeder::class,
+            ContentTransferSeeder::class,
             AgentPromptsSeeder::class,
         ]);
     }
